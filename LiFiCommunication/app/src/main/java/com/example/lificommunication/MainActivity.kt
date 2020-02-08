04d185fecc03301bd53d5b483b8cf90f8971edf1
nastya nastya <nastya5551996@mail.ru>
2020-02-08 14:30:02 +0500

    интерфейс, переход между активити

--- LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivity.kt
+++ LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lificommunication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    
+//открыть экран для режима отправки
+    fun SendActivity (view: View) {
+        val sendIntent = Intent(this, MainActivitySend::class.java)
+        startActivity(sendIntent)
+
+    }
+    //открыть экран для режима приема
+    fun RecieveActivity (view: View) {
+        val recieveIntent = Intent(this, MainActivityRecieve::class.java)
+        startActivity(recieveIntent)
+
+    }
+    fun SettingActivity (view: View) {
+        val SettingIntent = Intent(this, MainActivitySetting::class.java)
+        startActivity(SettingIntent)
+
+    }
+    //открыть экран для спраки
+    fun SpravkaActivity (view: View) {
+        val SpravkaIntent = Intent(this, MainActivitySpravka::class.java)
+        startActivity(SpravkaIntent)
+
+    }
+
 }
