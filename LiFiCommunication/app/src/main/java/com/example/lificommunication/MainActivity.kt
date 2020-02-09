7d4ec2ffd48726ca07cddf4f4e7defbe91f91da2
KAD <hlyzov@kitactive.ru>
2020-02-09 19:06:39 +0500

    Добавлен выход из справки в главное меню

--- LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivity.kt
+++ LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivity.kt
@@ -11,28 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-//открыть экран для режима отправки
+    //открыть экран для режима отправки
     fun SendActivity (view: View) {
         val sendIntent = Intent(this, MainActivitySend::class.java)
         startActivity(sendIntent)
-
     }
     //открыть экран для режима приема
     fun RecieveActivity (view: View) {
         val recieveIntent = Intent(this, MainActivityRecieve::class.java)
         startActivity(recieveIntent)
-
     }
     fun SettingActivity (view: View) {
         val SettingIntent = Intent(this, MainActivitySetting::class.java)
         startActivity(SettingIntent)
-
     }
     //открыть экран для спраки
     fun SpravkaActivity (view: View) {
         val SpravkaIntent = Intent(this, MainActivitySpravka::class.java)
         startActivity(SpravkaIntent)
-
     }
 
 }
--- LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivitySpravka.kt
+++ LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivitySpravka.kt
@@ -1,12 +1,19 @@
 package com.example.lificommunication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivitySpravka : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_spravka)
+    }
+    //открыть экран для режима отправки
+    fun SendMain (view: View) {
+        val sendMain = Intent(this, MainActivity::class.java)
+        startActivity(sendMain)
     }
 }
