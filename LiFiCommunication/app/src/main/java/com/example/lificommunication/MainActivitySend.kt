bd6f3c87a96a4f382d7806b07af07b7c0c8a0157
nastya nastya <nastya5551996@mail.ru>
2020-04-01 18:45:33 +0500

    Сохранение имени устройства и ключа безопасности, их шифрование и передача, передача в отдельном потоке

--- LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivitySend.kt
+++ LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivitySend.kt
@@ -1,5 +1,6 @@
 package com.example.lificommunication
 
+import android.content.Context
 import android.content.Intent
 import android.media.AudioAttributes
 import android.media.AudioAttributes.USAGE_MEDIA
@@ -9,20 +10,25 @@ import android.media.AudioFormat.ENCODING_PCM_16BIT
 import android.media.AudioManager.AUDIO_SESSION_ID_GENERATE
 import android.media.AudioTrack
 import android.os.Bundle
+import android.provider.Contacts
 import android.provider.OpenableColumns
-import android.provider.Settings
+import android.view.Gravity
 import android.view.View
+import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main_send.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.FileInputStream
+import java.io.InputStreamReader
 import java.util.*
-import kotlin.concurrent.thread
 import kotlin.experimental.xor
-import kotlinx.coroutines.*
 
 
+var ListArraySend = arrayListOf<BitSet>() //здесь храним посылки
 
-var ListArraySend = arrayListOf<BitSet>()
     class RC4 ( key: ByteArray){
         var Perestanovki= IntArray (256, {0})
         var x: Int = 0
@@ -46,10 +52,9 @@ var ListArraySend = arrayListOf<BitSet>()
                 Perestanovki[j]=temp
             }
         }
+
         init
-        {
-        InitParam(key)
-        }
+        { InitParam(key)}
         //генератор случайной последовательности
         fun keyRandom(): Int  {
             x=(x+1)%256
@@ -69,6 +74,7 @@ var ListArraySend = arrayListOf<BitSet>()
         }
             return chiperData
         }
+
         fun Decode(dataBinaryUsers: ByteArray, size: Int): ByteArray  {
 
             return Encode(dataBinaryUsers, size)
@@ -80,14 +86,45 @@ var ListArraySend = arrayListOf<BitSet>()
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main_send)
+            val fileInputDataUser: FileInputStream = openFileInput("dataUser.txt") // файл с именем уст-ва и паролем
+            val inputStreamFileUser = InputStreamReader(fileInputDataUser) // поток для чтения
+            val inputBuffer = CharArray(22) // максимальный размер буфера
+            inputStreamFileUser.read(inputBuffer) //запись из файла в буфер
+            val dataNamePassw = String(inputBuffer) // получили строку с данныими
+            var ArrayDataNamePasw=dataNamePassw.split(',')
+            var Passw=ArrayDataNamePasw[0].toByteArray() // ключ безопасности
+            var nameDevice=ArrayDataNamePasw[1].toByteArray() //имя устройства
+
+            var keyForPassw = "LightNamePassw".toByteArray() //это ключ для шифрования ключа пароля безопасности
+            var encoderKeyPassw: RC4=RC4(keyForPassw)
+            var encoderResultPassw=encoderKeyPassw.Encode(Passw, Passw.size) //шифрование пароля безопасности
+
+            var keyForNameDevice = "LightNameDevice".toByteArray() //это ключ для шифрования ключа имени устройства
+            var encoderKeyDevice: RC4=RC4(keyForNameDevice)
+            var encoderResultNameDevice=encoderKeyDevice.Encode(nameDevice, nameDevice.size) //шифрование имени устройства
+
+            packageCreate(encoderResultPassw) //формирование посылки для пароля безопасности
+            packageCreate(encoderResultNameDevice) // для имени устройства
+
+            val switchSend = findViewById<Switch>(R.id.switchSend) //проверка переключателя switch
+            if (switchSend != null) {
+                switchSend.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) {
+                        flagSend=true
+                    } else {
+                  flagSend=false
+                    }
+                }
+            }
         }
 
         val arrayFiles = arrayOfNulls<String>(3)
         var count: Int = 0
+        var flagSend: Boolean=true
+
         fun AddFile(view: View) {
             val intent = Intent().setType("*/*")
                 .setAction(Intent.ACTION_GET_CONTENT)//Выбор любого типа файла
-
             try {
                 startActivityForResult(Intent.createChooser(intent, "Выберите файл..."), 111)
             } catch (e: Exception) {
@@ -95,18 +132,45 @@ var ListArraySend = arrayListOf<BitSet>()
                     .show()
             }
         }
-
-        fun SendData(view: View) = runBlocking {
-            val taskSend = GlobalScope.launch {
+        fun SendData(view: View?) {
+            val taskSend = GlobalScope.launch() {
+            if (flagSend) {
                 for (n in ListArraySend) {
-                    SendInformation(n.toByteArray())
+                     if (flagSend)
+                    SendInformation(n.toByteArray()) // ОРГАНИЗОВАТЬ ОБРАЩЕНИЕ ПОТОКА К ПЕРЕМЕННОЙ
+                     /*    else {
+                         val showWarning = Toast.makeText(
+                             applicationContext,
+                             "Вы отключили режим передачи",
+                             Toast.LENGTH_SHORT
+                         )
+                         showWarning.setGravity(Gravity.CENTER, 0, 0)
+                         showWarning.show()
+                          break  } */
+                    }
                 }
             }
-            taskSend.join()
-        }
+            val taskSend1 = GlobalScope.launch() {
+                while (true) {
+                    if (!flagSend) {
+                           /*     val showWarning = Toast.makeText(
+                                    applicationContext,
+                                    "Вы отключили режим передачи",
+                                    Toast.LENGTH_SHORT
+                                )
+                                showWarning.setGravity(Gravity.CENTER, 0, 0)
+                                showWarning.show() */
+                                taskSend.cancel()
+                        break
+                            }
+                        }
+                    }
+                }
 
-        fun CrcPack (packData:BitSet): BitSet //подсчет crc и преобразование ее в биты
-        {
+
+
+
+        fun CrcPack (packData:BitSet): BitSet  {//подсчет crc и преобразование ее в биты
             var DataIntFormat :Int=0
             var Polinom:Int=98309
             for (n in 0..223) //в десятичное число данные
@@ -128,7 +192,6 @@ var ListArraySend = arrayListOf<BitSet>()
             return crcBit
         }
         fun packageCreate(dataUsers: ByteArray) {
-
            var countIndex: Int = 0
            var predel: Int = 0
            var count: Int = 0
@@ -146,12 +209,8 @@ var ListArraySend = arrayListOf<BitSet>()
 
            while (outCircle <= size) {
                if (size <= 1) //меньше одной посылки
-               {
-                   predel = dataUsers.size * 8
-               } //если меньше 110 бит
-               else {
-                   predel = 110 + (countIndex * outCircle)
-               }
+               { predel = dataUsers.size * 8 }
+               else { predel = 110 + (countIndex * outCircle) } //если меньше 110 бит
                for (n in (0 + countIndex * outCircle)..(109 + (countIndex * outCircle))) { //для одной посылки
                    if (n == 0 + countIndex * outCircle) {
                        sendPackTemp[count] = true //старт бит
@@ -160,13 +219,10 @@ var ListArraySend = arrayListOf<BitSet>()
                        count++
                    }
                    if (n > predel) { // если число битов меньше 110, то дополняем до 110 бит 0 и соответственно доп.единицами
-
                            sendPackTemp[count] = false //биты данных для дополнения посылки
                            count++
                            sendPackTemp[count] = true
                            count++
-
-
                    } else {
                        sendPackTemp[count] = sendPack[n] //бит данных
                        count++
@@ -177,8 +233,7 @@ var ListArraySend = arrayListOf<BitSet>()
                        }
                        count++
                    }
-                   if (n == 109 + countIndex * outCircle) //стоп-бит
-                   {
+                   if (n == 109 + countIndex * outCircle) { //стоп-бит
                        sendPackTemp[count] = false
                        count++
                        sendPackTemp[count] = true
@@ -189,39 +244,29 @@ var ListArraySend = arrayListOf<BitSet>()
                var crcForPack = CrcPack(sendPackTemp) //получили crc
                var sizeCircle = crcForPack.length()
                if (crcForPack.length() < 16) { //если crc меньше 16 бит
-                   for (n in 0..(16-crcForPack.length()-1)) //добавляем доп. нули чтобы дополнить ее до 16 бит
-                   {
+                   for (n in 0..(16-crcForPack.length()-1)) { //добавляем доп. нули чтобы дополнить ее до 16 бит
                        sendPackTemp[countThirdCircleForCrc] = false
                        countThirdCircleForCrc++
                        sendPackTemp[countThirdCircleForCrc] = true
-                       countThirdCircleForCrc++
-
-                   }
+                       countThirdCircleForCrc++ }
                }
-               for (n in 0..(sizeCircle)) //добавляем в посылку crc
-               {
+               for (n in 0..(sizeCircle)) { //добавляем в посылку crc
                    sendPackTemp[countThirdCircleForCrc] = crcForPack[n]
                    countThirdCircleForCrc++
-                   if (n == sizeCircle - 1) //стоп-бит
-                   {
+                   if (n == sizeCircle - 1) { //стоп-бит
                        sendPackTemp[countThirdCircleForCrc++] = false
                        countThirdCircleForCrc++
                        sendPackTemp[countThirdCircleForCrc++] = true
                    }
                }
-
                UnitPackSend = sendPackTemp
-
                ListArraySend.add(UnitPackSend) //добавляем в глобальную переменную преобразованные данные для дальнейшей отправки
-
            }
         }
-
-       suspend fun SendInformation(dataUsersFoJack: ByteArray) {
+        suspend fun SendInformation(dataUsersFoJack: ByteArray) {
             var buffersize = AudioTrack.getMinBufferSize(8000,  //устанавливаем частоту с запасом
-                AudioFormat.CHANNEL_OUT_MONO, // данные идут в левый канал, поэтому моно
+                AudioFormat.CHANNEL_OUT_FRONT_LEFT, // данные идут в левый канал, поэтому моно
                 AudioFormat.ENCODING_PCM_16BIT)
-
             var trackplayer = AudioTrack( AudioAttributes.Builder().setUsage(USAGE_MEDIA) //устанавливаем соответствующие настройки для объекта api
                 .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
                 .build(),
@@ -236,61 +281,52 @@ var ListArraySend = arrayListOf<BitSet>()
                trackplayer.write(dataUsersFoJack, 0,dataUsersFoJack.size) // пишем данные в цап мобильного
         }
 
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == 111 && resultCode == RESULT_OK) {
+                var selectedFile = data?.data //The uri with the location of the file
+                var matchResult = Regex("""([^%2F]*)$""").find(selectedFile.toString())
+                if (matchResult === null) return
+                if (count >= 3) {
+                    Toast.makeText(this, "Вы не можете выбрать больше трех файлов.", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                arrayFiles.set(count, selectedFile.toString())
+                count++
+                if (selectedFile!=null) //если выбраны файлы
+                { //получаем поток байт из  файла
+                    var fileUser=contentResolver.openInputStream(selectedFile)?.readBytes() //получаем поток байт из  файла
+                    if (fileUser!=null) //если файл не пустой, то:
+                    { //получаем имя и расширение
+                        var returnCursor=contentResolver.query(selectedFile, null, null, null, null);
+                        var nameIndex=returnCursor!!.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                        returnCursor.moveToFirst()
+                        var nameFile= returnCursor.getString(nameIndex) //имя и расширение вместе
 
-            override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-                super.onActivityResult(requestCode, resultCode, data)
+                        if(count === 1) InfoAddFiles.text = nameFile //вывод имени файла
+                        else {
+                            InfoAddFiles.append(System.getProperty("line.separator"))
+                            InfoAddFiles.append(nameFile)
+                        }
+                        var arrayNameExection=nameFile.split('.') //разбиваем на массив
+                        var nameFileStr=arrayNameExection[0].toByteArray() //имя
+                        var exectionFileStr=arrayNameExection[1].toByteArray() //расширение без точки
 
-                if (requestCode == 111 && resultCode == RESULT_OK) {
-                    var selectedFile = data?.data //The uri with the location of the file
-                    var matchResult = Regex("""([^%2F]*)$""").find(selectedFile.toString())
-                    if (matchResult === null) return
+                        var keyForUnitsName = "LightName".toByteArray(); //это ключ для шифрования имени файла
+                        var encoderN: RC4=RC4(keyForUnitsName)
+                        var encoderResultNameFile=encoderN.Encode(nameFileStr, nameFileStr.size) //шифрование имени файла
 
-                    if (count >= 3) {
-                        Toast.makeText(this, "Вы не можете выбрать больше трех файлов.", Toast.LENGTH_SHORT).show()
-                        return
-                    }
-                    arrayFiles.set(count, selectedFile.toString())
-                    count++
+                        var keyForUnitsFormat = "LightFormat".toByteArray(); //это ключ для шифрования формата файла
+                        var encoderF: RC4=RC4(keyForUnitsFormat)
+                        var encoderResultFormatFile=encoderF.Encode(exectionFileStr, exectionFileStr.size) //шифрование формата файла
 
+                        var keyForUnitsFile = "LightFile".toByteArray(); //это ключ для шифрования данных файла
+                        var encoderFile: RC4=RC4(keyForUnitsFile)
+                        var encoderResultFile=encoderFile.Encode(fileUser, fileUser.size) //шифрование файла
 
-                    if (selectedFile!=null) //если выбраны файлы
-                    { //получаем поток байт из  файла
-                        var fileUser=contentResolver.openInputStream(selectedFile)?.readBytes() //получаем поток байт из  файла
-
-                        if (fileUser!=null) //если файл не пустой, то:
-                        { //получаем имя и расширение
-                            var returnCursor=contentResolver.query(selectedFile, null, null, null, null);
-                            var nameIndex=returnCursor!!.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                            returnCursor.moveToFirst()
-                            var nameFile= returnCursor.getString(nameIndex) //имя и расширение вместе
-
-
-                            if(count === 1) InfoAddFiles.text = nameFile //вывод имени файла
-                            else {
-                                InfoAddFiles.append(System.getProperty("line.separator"))
-                                InfoAddFiles.append(nameFile)
-                            }
-
-                            var arrayNameExection=nameFile.split('.') //разбиваем на массив
-                            var nameFileStr=arrayNameExection[0].toByteArray() //имя
-                            var exectionFileStr=arrayNameExection[1].toByteArray() //расширение без точки
-
-
-                            var keyForUnitsName = "LightName".toByteArray(); //это ключ для шифрования имени файла
-                            var encoderN: RC4=RC4(keyForUnitsName)
-                            var encoderResultNameFile=encoderN.Encode(nameFileStr, nameFileStr.size) //шифрование имени файла
-
-                            var keyForUnitsFormat = "LightFormat".toByteArray(); //это ключ для шифрования формата файла
-                            var encoderF: RC4=RC4(keyForUnitsFormat)
-                            var encoderResultFormatFile=encoderF.Encode(exectionFileStr, exectionFileStr.size) //шифрование формата файла
-
-                            var keyForUnitsFile = "LightFile".toByteArray(); //это ключ для шифрования данных файла
-                            var encoderFile: RC4=RC4(keyForUnitsFile)
-                            var encoderResultFile=encoderFile.Encode(fileUser, fileUser.size) //шифрование файла
-
-                            packageCreate(encoderResultNameFile) //формирование посылки для имени
-                            packageCreate(encoderResultFormatFile) // для формата
-                            packageCreate(encoderResultFile) // для файла
+                        packageCreate(encoderResultNameFile) // формирование посылки для имени файла
+                        packageCreate(encoderResultFormatFile) // для формата
+                        packageCreate(encoderResultFile) // для файла
 
                                 /*схема дешифрования:
                             var decoder: RC4=RC4(keyForUnitsName)
@@ -307,9 +343,7 @@ var ListArraySend = arrayListOf<BitSet>()
                             var resultstrDecoderFile=decoderResult.toString(Charsets.UTF_8) //смотрим что получили после дешифрования
                             var t3=resultstrDecoderFile*/
                         }
-
                     }
-
                 }
             }
         }
--- LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivitySetting.kt
+++ LiFiCommunication/app/src/main/java/com/example/lificommunication/MainActivitySetting.kt
@@ -1,42 +1,96 @@
 package com.example.lificommunication
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main_setting.*
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 class MainActivitySetting : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fileInputDataUser: FileInputStream = openFileInput("dataUser.txt") // файл с именем уст-ва и паролем
+        val inputStreamFileUser = InputStreamReader(fileInputDataUser) //
+        val inputBuffer = CharArray(22)
+        inputStreamFileUser.read(inputBuffer)
+        val dataNamePassw = String(inputBuffer) // получили строку с данныими
+        var ArrayDataNamePasw=dataNamePassw.split(',')
         setContentView(R.layout.activity_main_setting)
+        editTextNameDevice.text.append(ArrayDataNamePasw[1])
+        editTextPassword.text.append(ArrayDataNamePasw[0])
     }
-
-    fun SaveDeviceSettings (view: View) {
-        //TODO::Добавить преобразование и шифрование пароля
+    //открыть экран для режима приема
+    fun MainActivityActivity (view: View) {
+        val MainIntent = Intent(this, MainActivity::class.java)
+        startActivity(MainIntent)
+    }
+    fun SaveDeviceSettings(view: View) {
         //получение имени устройства
         val deviceName = editTextNameDevice.text.toString()
-
-        if (deviceName.length > 16) {
-            val showWarning = Toast.makeText(this, "Имя устройства должно быть короче 17 символов.", Toast.LENGTH_SHORT)
+        if (deviceName.length > 10) {
+            val showWarning = Toast.makeText(
+                this,
+                "Имя устройства должно быть короче 11 символов.",
+                Toast.LENGTH_SHORT
+            )
+            showWarning.setGravity(Gravity.CENTER, 0, 0)
+            showWarning.show()
+            return
+        } else if (deviceName.length < 4) {
+            val showWarning = Toast.makeText(
+                this,
+                "Имя устройства должно быть длиннее 3 символов.",
+                Toast.LENGTH_SHORT
+            )
+            showWarning.setGravity(Gravity.CENTER, 0, 0)
             showWarning.show()
             return
         }
-
         //получение пароля для передачи
         val devicePassword = editTextPassword.text.toString()
-
-        if (devicePassword.length < 8) {
-            val showWarning = Toast.makeText(this, "Пароль устройства должен быть длинее 8 символов.", Toast.LENGTH_SHORT)
+        if (devicePassword.length < 5) {
+            val showWarning = Toast.makeText(
+                this,
+                "Пароль устройства должен быть длинее 4 символов.",
+                Toast.LENGTH_SHORT
+            )
+            showWarning.setGravity(Gravity.CENTER, 0, 0)
+            showWarning.show()
+            return
+        } else if (devicePassword.length > 10) {
+            val showWarning = Toast.makeText(
+                this,
+                "Пароль устройства должен быть менее 11 символов.",
+                Toast.LENGTH_SHORT
+            )
+            showWarning.setGravity(Gravity.CENTER, 0, 0)
             showWarning.show()
             return
         }
-
-        //Проверка корректности полученных данных для нас
-        /*val showName = Toast.makeText(this, deviceName, Toast.LENGTH_SHORT)
+            val fileOut: FileOutputStream = openFileOutput("dataUser.txt", Context.MODE_PRIVATE)
+            val outputStreamFile = OutputStreamWriter(fileOut)
+            outputStreamFile.write(devicePassword + "," + deviceName+",")  // записываем строку c именем устройства и паролем  в файл
+            outputStreamFile.flush()// проверяем, что все записалось в файл
+            outputStreamFile.close() //закрываем поток
+        val showWarning = Toast.makeText(
+            this,
+            "Данные успешно сохранены!",
+            Toast.LENGTH_SHORT
+        )
+        showWarning.setGravity(Gravity.CENTER, 0, 0)
+        showWarning.show()
+            //Проверка корректности полученных данных для нас
+            /*val showName = Toast.makeText(this, deviceName, Toast.LENGTH_SHORT)
         showName.show()
         val showPass = Toast.makeText(this, devicePassword, Toast.LENGTH_SHORT)
         showPass.show()*/
+
     }
 }
